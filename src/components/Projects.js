1fb03eefa839ee979466154ce48869a3af4b4c90
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-10-30 22:41:38 +0000

    tutorial pill added

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import projectImg1 from "../assets/img/SGARhome.png";
 import projectImg2 from "../assets/img/BrewDogHome.png";
 import ProjectCard from "./ProjectCard";
@@ -6,12 +6,14 @@ import projectImg3 from "../assets/img/ServivorsHome.png";
 import projectImg4 from "../assets/img/WiggleWaggyWalks.png";
 import projectImg5 from "../assets/img/LouLousLibrary.png";
 import projectImg6 from "../assets/img/tennis_order.png";
+import tutorialImg1 from "../assets/img/movieland.png"
+import tutorialImg2 from "../assets/img/iphonegsap.png"
+import tutorialImg3 from "../assets/img/sunsister.png"
+
+
 import video1 from "../assets/img/www.mp4";
 import video2 from "../assets/img/stc.mp4";
 import video3 from "../assets/img/lll.mp4";
-
-
-
 
 import colorSharp from "../assets/img/color-sharp.png";
 
@@ -41,7 +43,6 @@ const Projects = () => {
       imgUrl: projectImg4,
       showPopUp: "VIEW DEMO",
       videoUrl: video1,
-      
     },
     {
       title: "SERVivors Tennis Club Booking System",
@@ -51,7 +52,7 @@ const Projects = () => {
       showPopUp: "VIEW DEMO",
       videoUrl: video2,
     },
-    
+
     {
       title: "Lou Lou's Library",
       description: "Solo Project - July 23",
@@ -66,7 +67,33 @@ const Projects = () => {
       stack: "Python, Flask",
       imgUrl: projectImg6,
     },
-    
+  ];
+
+  const tutorials = [
+    {
+      title: "MovieLand",
+      description: "JS Mastery React Tutorial",
+      stack: "React, API integration, handling JSON responses",
+      imgUrl: tutorialImg1,
+      link: "https://movie-app-blush-five-72.vercel.app/",
+      viewApp: "PLAY APP",
+    },
+    {
+      title: "IPhone 15 Pro Clone",
+      description: "JS Mastery Tutorial",
+      stack: "React, GSAP animations, Three.JS, Sentry",
+      imgUrl: tutorialImg2,
+      link: "https://apple-threejs.vercel.app",
+      viewApp: "PLAY APP",
+    },
+    {
+      title: "Mybrary",
+      description: "Web Dev Simplified - Full Stack Web Development Course",
+      stack: "Node.js, Express.js & MongoDB",
+      imgUrl: tutorialImg3,
+      link: "https://mybraryjs.fly.dev/",
+      viewApp: "PLAY APP",
+    },
 
   ];
 
@@ -75,15 +102,35 @@ const Projects = () => {
       <Container>
         <Row>
           <Col>
-            <h2>Projects</h2>
-            <p>
-              A selection of projects completed during my CodeClan Course
-            </p>
-            <Row>
-              {projects.map((project, index) => {
-                return <ProjectCard key={index} {...project} />;
-              })}
-            </Row>
+            <h2>Projects & Tutorials</h2>
+            <div className="tab-container">
+              <Tabs
+                defaultActiveKey="projects"
+                id="project-tutorial-tabs"
+                variant="pills"
+                className="pills"
+              >
+                <Tab id="tabs" eventKey="projects" title="Projects">
+                  <p>Personal and group projects</p>
+
+                  <Row>
+                    {projects.map((project, index) => {
+                      return <ProjectCard key={index} {...project} />;
+                    })}
+                  </Row>
+                </Tab>
+                <Tab id="tab" eventKey="tutorials" title="Tutorials">
+                  <p>A selection of tutorials I have followed</p>
+
+                  <Row>
+                    {tutorials.map((tutorial, index) => {
+                      return <ProjectCard key={index} {...tutorial} />;
+                    })}
+                  </Row>
+                </Tab>
+                <Col sm={9}></Col>
+              </Tabs>
+            </div>
           </Col>
         </Row>
       </Container>
