ef543b754888bbf19ecb7b04c53e167071441861
gillianroberts1 <138917227+gillianroberts1@users.noreply.github.com>
2024-11-03 19:22:25 +0000

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/github-mark.svg";
+import { sendEmail } from "../helpers";
 
-const Footer = () => {
+import "./Footer.css";
+
+export const Footer = () => {
   return (
-    <div>
-        <p>© 2024 CodeCarmencita</p>
-    </div>
-  )
-}
+    <footer className="footer">
+      <p>© 2024 CodeCarmenCita. All Rights Reserved.</p>
 
-export default Footer
+      <div className="social-icon">
+        <a
+          href="https://www.linkedin.com/in/gillian-roberts-4293804a/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={navIcon1} alt="LinkedIn" />
+        </a>
+        <a
+          href="https://github.com/gillianroberts1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={navIcon2} alt="Github" />
+        </a>
+      </div>
+      <button className="connect" onClick={sendEmail}>
+        <span>Let'Connect</span>
+      </button>
+    </footer>
+  );
+};
+
+export default Footer;
